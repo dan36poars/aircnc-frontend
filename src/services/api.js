461730d3d7c5e0c63666b52ddca3d8fca6000b04
Daniel Corrêa <eldestemido@gmail.com>
2019-10-09 09:16:02 -0300

    api url change

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const env = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : process.env.REACT_APP_API_URL;
+const env = process.env.NODE_ENV === 'development' ? 'http://192.168.0.3:3333' : process.env.REACT_APP_API_URL;
 
+console.log(env);
 const api = axios.create({
 	baseURL: env
 });
